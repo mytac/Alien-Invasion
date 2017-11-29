e4283d2962acf7f2a898ed61edc003754d39dfd9
张君卓 <my_tac@126.com>
2017-11-29 14:45:14 +0800

    :sparkles:aliens向下移动

--- game_functions.py
+++ game_functions.py
@@ -90,6 +90,11 @@ def create_fleet(ai_settings,screen,ship,aliens):
 		for alien_number in range(number_aliens_x):
 			create_alien(ai_settings,screen,aliens,alien_number,row_number)
 		
+def update_aliens(ai_settings,aliens):
+	"""检查是否有外星人位于屏幕边缘，并更新整群外星人的位置"""
+	check_fleet_edges(ai_settings,aliens)
+	"""更新外星人群中所有外星人的位置"""
+	aliens.update()
 
 def update_bullets(bullets):
 	"""更新子弹的位置，并删除已消失的子弹"""
@@ -99,3 +104,17 @@ def update_bullets(bullets):
 	for bullet in bullets.copy():
 		if bullet.rect.bottom<=0:
 			bullets.remove(bullet)
+
+def check_fleet_edges(ai_settings,aliens):
+	"""有外星人到达边缘时采取相应的措施"""
+	for alien in aliens.sprites():
+		if alien.check_edges():
+			change_fleet_direction(ai_settings,aliens)
+			break
+
+def change_fleet_direction(ai_settings,aliens):
+	"""将整群外星人下移，并改变他们的方向"""
+	for alien in aliens.sprites():
+		alien.rect.y+=ai_settings.fleet_drop_speed
+	ai_settings.fleet_direction*=-1
+

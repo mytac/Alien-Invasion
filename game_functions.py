a6897e0eff4c1798f6da68af090365d626b5e0d3
张君卓 <my_tac@126.com>
2017-12-05 17:23:26 +0800

    修改速度配置

--- game_functions.py
+++ game_functions.py
@@ -38,6 +38,8 @@ def check_play_button(ai_settings,screen,stats,play_button,ship,aliens,bullets,m
 	"""在玩家单击play按钮时开始新游戏"""
 	button_clicked=play_button.rect.collidepoint(mouse_x,mouse_y)
 	if button_clicked and not stats.game_active:
+		# 重置游戏设置
+		ai_settings.initialize_dynamic_settings()
 		# 隐藏光标
 		pygame.mouse.set_visible(False)
 		# 重置游戏统计信息
@@ -157,8 +159,9 @@ def check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets):
 	collisions=pygame.sprite.groupcollide(bullets,aliens,True,True)
 
 	if len(aliens)==0:
-		#删除现有的子弹并新建一群外星人
+		#删除现有的子弹,加快节奏并新建一群外星人
 		bullets.empty()
+		ai_settings.increase_speed()
 		create_fleet(ai_settings,screen,ship,aliens)
 
 def update_bullets(ai_settings,screen,ship,aliens,bullets):
